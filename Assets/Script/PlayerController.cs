7f03089f843440d4be71186a1f32a8ad49a94585
李庠霖 <snowmelt945@gmail.com>
2023-04-22 21:22:40 +0800

    VFX & Aim Assets

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public GameObject TestGameObject;
     private void Update()
     {
-        
+        cameraDirectionTest();
     }
 
     private void cameraDirectionTest()
     {
-
+        Vector3 Direction = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
+        TestGameObject.transform.eulerAngles = Direction;
     }
-
 }
