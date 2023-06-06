feb736008681638242d571f22df4b49919d22860
李庠霖 <snowmelt945@gmail.com>
2023-06-06 19:02:06 +0800

    DemoEnd 事件製作完成

--- Assets/TestTool/DemoEnd.cs
+++ Assets/TestTool/DemoEnd.cs
@@ -7,9 +7,11 @@ public class DemoEnd : MonoBehaviour
 {
     [SerializeField] private int waitTime;
     [SerializeField] private GameObject FogWall;
+    [SerializeField] private Animator DemoEndAnimator;
     private async void OnTriggerEnter(Collider other)
     {
         await Task.Delay(waitTime * 1000);
         FogWall.SetActive(false);
+        DemoEndAnimator.Play("DemoEnd");
     }
 }
