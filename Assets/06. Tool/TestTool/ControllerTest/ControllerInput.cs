54216e9b1f6bbf57088ca6168f7cd48b5e432a36
李庠霖 <snowmelt945@gmail.com>
2023-06-20 18:47:31 +0800

    Controller | Game Pad | All

---
+++ Assets/06. Tool/TestTool/ControllerTest/ControllerInput.cs
@@ -0,0 +1,165 @@
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+    [Header("Controller Input Buttons")]
+    public bool ButtonY;
+    public bool ButtonX;
+    public bool ButtonA;
+    public bool ButtonB;
+
+    [Header("D-Pad")]
+    public bool ArrowKeyUp;
+    public bool ArrowKeyLeft;
+    public bool ArrowKeyDown;
+    public bool ArrowKeyRight;
+
+    [Header("L")]
+    public bool LT;
+    public bool LB;
+    public bool LSB;
+
+    [Header("R")]
+    public bool RT;
+    public bool RB;
+    public bool RSB;
+
+    [Header("Setting")]
+    public bool Option;
+    public bool Window;
+
+    #region InputSystem_Input
+    public void OnController_Button_A(InputValue value)
+    {
+        Input_Button_A(value.isPressed);
+    }
+    public void OnController_Button_B(InputValue value)
+    {
+        Input_Button_B(value.isPressed);
+    }
+    public void OnController_Button_X(InputValue value)
+    {
+        Input_Button_X(value.isPressed);
+    }
+    public void OnController_Button_Y(InputValue value)
+    {
+        Input_Button_Y(value.isPressed);
+    }
+    public void OnController_ArrowKey_Up(InputValue value)
+    {
+        Input_Arrow_Up(value.isPressed);
+    }
+    public void OnController_ArrowKey_Left(InputValue value)
+    {
+        Input_Arrow_Left(value.isPressed);
+    }
+    public void OnController_ArrowKey_Down(InputValue value)
+    {
+        Input_Arrow_Down(value.isPressed);
+    }
+    public void OnController_ArrowKey_Right(InputValue value)
+    {
+        Input_Arrow_Right(value.isPressed);
+    }
+    public void OnController_Right_Trigger(InputValue value)
+    {
+        Input_Right_Trigger(value.isPressed);
+    }
+    public void OnController_Right_Button(InputValue value)
+    {
+        Input_Right_Button(value.isPressed);
+    }
+    public void OnController_Right_StickButton(InputValue value)
+    {
+        Input_Right_StickButton(value.isPressed);
+    }
+    public void OnController_Left_Trigger(InputValue value)
+    {
+        Input_Left_Trigger(value.isPressed);
+    }
+    public void OnController_Left_Button(InputValue value)
+    {
+        Input_Left_Button(value.isPressed);
+    }
+    public void OnController_Left_StickButton(InputValue value)
+    {
+        Input_Left_StickButton(value.isPressed);
+    }
+    public void OnController_Option(InputValue value)
+    {
+        Input_Option(value.isPressed);
+    }
+    public void OnController_Window(InputValue value)
+    {
+        Input_Window(value.isPressed);
+    }
+    #endregion
+    #region Button_Input
+    public void Input_Button_A(bool newButtonState)
+    {
+        ButtonA = newButtonState;
+    }
+    public void Input_Button_B(bool newButtonState)
+    {
+        ButtonB = newButtonState;
+    }
+    public void Input_Button_X(bool newButtonState)
+    {
+        ButtonX = newButtonState;
+    }
+    public void Input_Button_Y(bool newButtonState)
+    {
+        ButtonY = newButtonState;
+    }
+    public void Input_Arrow_Up(bool newButtonState)
+    {
+        ArrowKeyUp = newButtonState;
+    }
+    public void Input_Arrow_Left(bool newButtonState)
+    {
+        ArrowKeyLeft = newButtonState;
+    }
+    public void Input_Arrow_Down(bool newButtonState)
+    {
+        ArrowKeyDown = newButtonState;
+    }
+    public void Input_Arrow_Right(bool newButtonState)
+    {
+        ArrowKeyRight = newButtonState;
+    }
+    public void Input_Right_Trigger(bool newButtonState)
+    {
+        RT = newButtonState;
+    }
+    public void Input_Right_Button(bool newButtonState)
+    {
+        RB = newButtonState;
+    }
+    public void Input_Right_StickButton(bool newButtonState)
+    {
+        RSB = newButtonState;
+    }
+    public void Input_Left_Trigger(bool newButtonState)
+    {
+        LT = newButtonState;
+    }
+    public void Input_Left_Button(bool newButtonState)
+    {
+        LB = newButtonState;
+    }
+    public void Input_Left_StickButton(bool newButtonState)
+    {
+        LSB = newButtonState;
+    }
+    public void Input_Option(bool newButtonState)
+    {
+        Option = newButtonState;
+    }
+    public void Input_Window(bool newButtonState)
+    {
+        Window = newButtonState;
+    }
+    #endregion
+}
--- Assets/06. Tool/TestTool/ControllerTest/ControllerTest.cs
+++ Assets/06. Tool/TestTool/ControllerTest/ControllerTest.cs
@@ -1,21 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using StarterAssets;
 using UnityEngine.UI;
-using System.Runtime.CompilerServices;
+using UnityEditor.PackageManager.UI;
 
 public class ControllerTest : MonoBehaviour
 {
     [SerializeField] private StarterAssetsInputs _Inputs;
+    [SerializeField] private ControllerInput _ControllerInput;
     [SerializeField] private ThirdPersonController thirdPersonController;
 
     [Header("Stick")]
-    [SerializeField] private RectTransform LeftStickPoint;
-    [SerializeField] private RectTransform RightStickPoint;
+    [SerializeField] private RectTransform LSB_transform;
+    [SerializeField] private RectTransform RSB_transform;
+
+    [Header("Right")]
+    [SerializeField] private Image RT;
+    [SerializeField] private Image RB;
+    [SerializeField] private Image RSB;
+
+    [Header("Left")]
+    [SerializeField] private Image LT;
+    [SerializeField] private Image LB;
+    [SerializeField] private Image LSB;
 
     [Header("Button")]
-    [SerializeField] private Image ButtonSouth;
+    [SerializeField] private Image ButtonY;
+    [SerializeField] private Image ButtonX;
+    [SerializeField] private Image ButtonA;
+    [SerializeField] private Image ButtonB;
+
+    [Header("Arrow")]
+    [SerializeField] private Image Arrow_Up;
+    [SerializeField] private Image Arrow_Left;
+    [SerializeField] private Image Arrow_Down;
+    [SerializeField] private Image Arrow_Right;
+
+    [Header("Setting")]
+    [SerializeField] private Image Option;
+    [SerializeField] private Image Window;
     private Vector2 originLeftStickPoint;
     private Vector2 originRightStickPoint;
 
@@ -27,22 +49,49 @@ public class ControllerTest : MonoBehaviour
     {
         OnLeftStick();
         OnRightStick();
+        OnButtonA();
+        OnButtonB();
+        OnButtonX();
+        OnButtonY();
+        OnArrowUp();
+        OnArrowLeft();
+        OnArrowDown();
+        OnArrowRight();
+        OnRightTrigger();
+        OnRightButton();
+        OnRightStickButton();
+        OnLeftTrigger();
+        OnLeftButton();
+        OnLeftStickButton();
+        OnOption();
+        OnWindow();
     }
 
     private void getOriginStickPoint()
     {
-        originLeftStickPoint = LeftStickPoint.anchoredPosition;
-        originRightStickPoint = RightStickPoint.anchoredPosition;
+        originLeftStickPoint = LSB_transform.anchoredPosition;
+        originRightStickPoint = RSB_transform.anchoredPosition;
     }
-    private void OnLeftStick() => LeftStickPoint.anchoredPosition = originLeftStickPoint + (_Inputs.move * 90f);
+    private void OnLeftStick() => LSB_transform.anchoredPosition = originLeftStickPoint + (_Inputs.move * 90f);
     private void OnRightStick()
     {
         Vector2 realPoint = new Vector2(_Inputs.look.x,- _Inputs.look.y);
-        RightStickPoint.anchoredPosition = originRightStickPoint + (realPoint / 3.3f);
+        RSB_transform.anchoredPosition = originRightStickPoint + (realPoint / 3.3f);
     }
-    private void OnButtonSouth()
-    {
-        
-    }
-
+    private void OnButtonA() => ButtonA.color = _ControllerInput.ButtonA ? Color.green : Color.white;
+    private void OnButtonB() => ButtonB.color = _ControllerInput.ButtonB ? Color.red : Color.white;
+    private void OnButtonX() => ButtonX.color = _ControllerInput.ButtonX ? Color.blue : Color.white;
+    private void OnButtonY() => ButtonY.color = _ControllerInput.ButtonY ? Color.yellow : Color.white;
+    private void OnArrowUp() => Arrow_Up.color = _ControllerInput.ArrowKeyUp ? Color.blue : Color.white;
+    private void OnArrowLeft() => Arrow_Left.color = _ControllerInput.ArrowKeyLeft ? Color.blue : Color.white;
+    private void OnArrowDown() => Arrow_Down.color = _ControllerInput.ArrowKeyDown ? Color.blue : Color.white;
+    private void OnArrowRight() => Arrow_Right.color = _ControllerInput.ArrowKeyRight ? Color.blue : Color.white;
+    private void OnRightTrigger() => RT.color = _ControllerInput.RT ? Color.blue : Color.white;
+    private void OnRightButton() => RB.color = _ControllerInput.RB ? Color.blue : Color.white;
+    private void OnRightStickButton() => RSB.color = _ControllerInput.RSB ? Color.blue : Color.white;
+    private void OnLeftTrigger() => LT.color = _ControllerInput.LT ? Color.blue : Color.white;
+    private void OnLeftButton() => LB.color = _ControllerInput.LB ? Color.blue : Color.white;
+    private void OnLeftStickButton() => LSB.color = _ControllerInput.LSB ? Color.blue : Color.white;
+    private void OnOption() => Option.color = _ControllerInput.Window ? Color.blue : Color.white;
+    private void OnWindow() => Window.color = _ControllerInput.Option ? Color.blue : Color.white;
 }
