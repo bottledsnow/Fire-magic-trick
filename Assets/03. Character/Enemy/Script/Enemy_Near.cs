464f72f0278a0870056c3fdaa0e4fb2684786ce7
Anping6630 <anping6630@gmail.com>
2023-07-17 20:41:39 +0800

    修復敵人暴衝、不再看向天上 ( 僅修復Enemy_Near )

--- Assets/03. Character/Enemy/Script/Enemy_Near.cs
+++ Assets/03. Character/Enemy/Script/Enemy_Near.cs
@@ -6,18 +6,21 @@ public class Enemy_Near : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed;
-    [Header("Attack")]
-    public float attackRange = 1f;
-    public float attackDamage = 10f;
-    public float attackCooldown = 1f;
+    [Header("NormalAttack")]
+    public float normalAttack_Range = 1f;
+    public float normalAttack_Damage = 10f;
+    public float normalAttack_Cooldown = 3f;
+    public float normalAttack_Duration = 1.5f;
 
     private Transform playerTransform;
     private Rigidbody rb;
     private float distanceToPlayer;
+    private bool isMoveToPlayer;
+    private float normalAttack_FireTimer;
 
     private enum EnemyState
     {
-        Idle, Attack
+        Idle, Attack, 
     }
 
     private EnemyState currentState = EnemyState.Idle;
@@ -29,16 +32,19 @@ public class Enemy_Near : MonoBehaviour
         fov = this.gameObject.GetComponent<FieldOfView>();
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody>();
+        normalAttack_FireTimer = 0;
     }
 
     void Update()
     {
         distanceToPlayer = Vector3.Distance(transform.position, playerTransform.position);
+        SpeedControl();
 
         switch (currentState)
         {
             case EnemyState.Idle:
                 // 閒置狀態，等待發現玩家
+                isMoveToPlayer = false;
                 if (fov.canSeePlayer)
                 {
                     currentState = EnemyState.Attack;
@@ -47,25 +53,49 @@ public class Enemy_Near : MonoBehaviour
 
             case EnemyState.Attack:
                 // 近戰腳腳攻擊
-                if (distanceToPlayer < attackRange)
+                if (distanceToPlayer < normalAttack_Range && normalAttack_FireTimer >= normalAttack_Cooldown)
                 {
-                    transform.LookAt(playerTransform);
-
-                    // 近戰攻擊邏輯運算
-                    print("腳腳怪使出近戰攻擊!");
+                    normalAttack_FireTimer = 0;
+                    StartCoroutine(NormalAttack());
                 }
                 else
                 {
-                    MoveToPlayer();
+                    transform.LookAt(new Vector3(playerTransform.position.x,transform.position.y,playerTransform.position.z));
+                    isMoveToPlayer = true;
                 }
                 break;
         }
     }
 
+    void FixedUpdate()
+    {
+        if(isMoveToPlayer)
+        {
+            MoveToPlayer();
+        }
+
+        normalAttack_FireTimer += 0.2f;
+    }
+
     void MoveToPlayer()
     {
-        transform.LookAt(playerTransform);
+        transform.LookAt(new Vector3(playerTransform.position.x,transform.position.y,playerTransform.position.z));
         Vector3 direction = (playerTransform.position - transform.position).normalized;
         rb.AddForce(direction * moveSpeed);
+    }
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+    IEnumerator NormalAttack()
+    {
+        isMoveToPlayer = false;
+        yield return new WaitForSeconds(normalAttack_Duration);
     }
 }
