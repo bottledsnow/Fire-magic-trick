43e49c59c2530b84e84cf2674b6c65eb7b04834d
李庠霖 <snowmelt945@gmail.com>
2023-07-19 22:40:54 +0800

    火焰牌燃燒(30%)

---
+++ Assets/03. Character/Player/Skill/FireAsh/AshBurning.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class AshBurning : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+}
--- Assets/03. Character/Player/Skill/FireDash/FireDash_Collider.cs
+++ Assets/03. Character/Player/Skill/FireDash/FireDash_Collider.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-
 public class FireDash_Collider : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
