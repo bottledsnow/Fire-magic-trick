4695e9e96036b701d04f8a76dc5788a5fbe3c798
Anping6630 <anping6630@gmail.com>
2023-09-13 20:52:00 +0800

    Melee / Ranged / Air 敵人

--- Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/RangedShoot.cs
+++ Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/RangedShoot.cs
@@ -2,37 +2,38 @@ using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 
-public class SkyShoot : Action
+public class RangedShoot : Action
 {
     public float attackDuaction;
     public float rotateSpeed = 5;
     public SharedGameObject targetObject;
-	public Transform shootPoint;
-    public Vector3 targetPoint;
+    public Transform aimmingLinePoint;
+    public float playerHeight = 1.3f;
+    private Vector3 targetPoint;
     private float timer;
-	private LineRenderer lineRenderer;
+    private LineRenderer lineRenderer;
 
     public override void OnStart()
     {
         timer = Time.time;
-		lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.enabled = true;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = true;
     }
 
     public override TaskStatus OnUpdate()
     {
-		if (Time.time - timer <= attackDuaction - 0.5f)
+        if (Time.time - timer <= attackDuaction - 0.5f)
         {
-            Debug.Log("射擊蓄力");
-			_LookAtTarget();
-			targetPoint = targetObject.Value.transform.position;
-			lineRenderer.SetPosition(0, shootPoint.position);
-			lineRenderer.SetPosition(1, targetPoint);
+            //Debug.Log("射擊蓄力");
+            _LookAtTarget();
+            targetPoint = new Vector3(targetObject.Value.transform.position.x, targetObject.Value.transform.position.y + playerHeight / 2, targetObject.Value.transform.position.z);
+            lineRenderer.SetPosition(0, aimmingLinePoint.position);
+            lineRenderer.SetPosition(1, targetPoint);
         }
         if (Time.time - timer >= attackDuaction)
         {
-            Debug.Log("空中射擊");
-			lineRenderer.enabled = false;
+            //Debug.Log("空中射擊");
+            lineRenderer.enabled = false;
             return TaskStatus.Success;
         }
         return TaskStatus.Running;
