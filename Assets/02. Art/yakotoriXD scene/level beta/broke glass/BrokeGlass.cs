6d7ace5d002fbb06a2df9076b071508370a72b43
bottledsnow <snowmelt945@gmail.com>
2023-09-17 15:29:22 +0800

    玻璃碎裂

---
+++ Assets/02. Art/yakotoriXD scene/level beta/broke glass/BrokeGlass.cs
@@ -0,0 +1,47 @@
+using MoreMountains.Feedbacks;
+using UnityEngine;
+
+public class BrokeGlass : MonoBehaviour
+{
+    [SerializeField] private Rigidbody[] glasses;
+    [SerializeField] private float force = 100f;
+    [SerializeField] private MMF_Player Feedbacks;
+
+    private void Start()
+    {
+        GetAllChild();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            Broke();
+        }
+    }
+    private void GetAllChild()
+    {
+        Transform parentTransform = transform;
+
+        int childCount = parentTransform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            Transform childTransform = parentTransform.GetChild(i);
+            GameObject childGameObject = childTransform.gameObject;
+            glasses[i] = childGameObject.GetComponent<Rigidbody>();
+        }
+    }
+    public void Broke()
+    {
+        Debug.Log("Broke");
+        Feedbacks.PlayFeedbacks();
+        for (int i = 0;i < glasses.Length;i++)
+        {
+            if (glasses[i] != null)
+            {
+                glasses[i].useGravity = true;
+                glasses[i].isKinematic = false;
+                glasses[i].AddExplosionForce(force, transform.position, 25f);
+            }
+        }
+    }
+}
