83ccdbb474679543f3e58c66fda580e8697d0409
bottledsnow <snowmelt945@gmail.com>
2023-11-02 13:50:24 +0800

    場景轉換問題

--- Assets/01. Scenes/Release/Script/MenuMannager.cs
+++ Assets/01. Scenes/Release/Script/MenuMannager.cs
@@ -1,38 +1,33 @@
 using UnityEngine;
-using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
-using UnityEngine.Timeline;
 
 public class MenuMannager : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
-    [SerializeField] private PlayableDirector timeline;
     private ControllerInput _input;
-
+    [SerializeField] private GameObject thisCanva;
+    [SerializeField] private GameObject Player;
     private bool trigger;
     private void Start()
     {
         _input = GetComponent<ControllerInput>();
     }
-    private void Update()
-    {
-        CheckInput();
-    }
-    private void CheckInput()
-    {
-        if(_input.anyKey && !trigger)
-        {
-            InputEvent();
-            trigger = true;
-        }
-    }
-    private void InputEvent()
-    {
-        animator.Play("Menu");
-        timeline.Play();
-    }
+    
     public void LoadStartSence()
     {
-        SceneManager.LoadScene("StarBall");
+        SceneManager.LoadScene(1);
+        //thisCanva.SetActive(false);
+        //Player.SetActive(true);
+    }
+    public void LoadLevelChose()
+    {
+
+    }
+    public void LoadCreator()
+    {
+
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
