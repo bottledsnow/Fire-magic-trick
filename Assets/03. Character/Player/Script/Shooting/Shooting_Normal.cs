674161633464f81bae2f6aa17461e1858cb4dd9a
李庠霖 <snowmelt945@gmail.com>
2023-11-10 15:11:50 +0800

    丟擲VFX

--- Assets/03. Character/Player/Script/Shooting/Shooting_Normal.cs
+++ Assets/03. Character/Player/Script/Shooting/Shooting_Normal.cs
@@ -11,12 +11,15 @@ public class Shooting_Normal : MonoBehaviour
     [SerializeField] private float shootCooldown;
     [Header("Throw Feedbacks")]
     [SerializeField] private bool useThrowFeedbacks;
-    [SerializeField] private Transform ThrowParticle;
+    [SerializeField] private MMF_Player Throw_A;
+    [SerializeField] private MMF_Player Throw_B;
+    [SerializeField] private MMF_Player Throw_C;
 
     private CrosshairUI _crosshairUI;
     private Shooting _shooting;
     private ControllerInput _Input;
     private bool shooting;
+    private int ThrowFeedbacksIndex;
     private void Start()
     {
         _Input = GameManager.singleton._input;
@@ -31,8 +34,7 @@ public class Shooting_Normal : MonoBehaviour
     {
         if (_Input.RT && !shooting)
         {
-            ThrowFeedbacksRotation();
-            ThrowFeedback.PlayFeedbacks();
+            ThrowFeedbacks();
             _shooting.Shoot_Normal(pfBulletProjectile);
             _crosshairUI.CrosshairShooting();
             ShootCooldown(shootCooldown);
@@ -44,10 +46,30 @@ public class Shooting_Normal : MonoBehaviour
         await Task.Delay((int)(shootCooldown * 1000));
         shooting = false;
     }
-    private void ThrowFeedbacksRotation()
+    private void ThrowFeedbacks()
     {
         if(useThrowFeedbacks)
         {
+            if(ThrowFeedbacksIndex == 0)
+            {
+                Throw_A.PlayFeedbacks();
+            }
+            if (ThrowFeedbacksIndex == 1)
+            {
+                Throw_B.PlayFeedbacks();
+            }
+            if (ThrowFeedbacksIndex == 2)
+            {
+                Throw_C.PlayFeedbacks();
+            }
+
+            if (ThrowFeedbacksIndex == 2)
+            {
+                ThrowFeedbacksIndex = 0;
+            }else
+            {
+                ThrowFeedbacksIndex++;
+            }
             
         }
     }
