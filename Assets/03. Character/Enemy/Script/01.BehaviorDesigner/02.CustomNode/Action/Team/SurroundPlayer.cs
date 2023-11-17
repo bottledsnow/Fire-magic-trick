c76abbf25e8ffa929d6d2aabb645388588896a26
Anping6630 <anping6630@gmail.com>
2023-11-17 14:08:48 +0800

    包圍小修正

--- Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/Team/SurroundPlayer.cs
+++ Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/Team/SurroundPlayer.cs
@@ -19,14 +19,15 @@ public class SurroundPlayer : Action
     public override TaskStatus OnUpdate()
     {
         Vector3 targetPosition = CalculatePositionAroundPlayer(targetObject.Value.transform.position, surroundingNumber.Value);
-        Debug.Log(targetPosition);
         Movement(targetPosition);
         return TaskStatus.Success;
     }
 
     Vector3 CalculatePositionAroundPlayer(Vector3 playerPosition, int index)
     {
-        float angle = index * 2 * Mathf.PI / teammateEnemies.Value.Count;
+        index++;
+        int count = teammateEnemies.Value.Count + 4;
+        float angle = index * 2 * Mathf.PI / count;
         Vector3 offset = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * circleRadius;
 
         return playerPosition + offset;
@@ -35,6 +36,7 @@ public class SurroundPlayer : Action
     void Movement(Vector3 targetPosition)
     {
         Vector3 direction = (targetPosition - rb.position).normalized;
+        direction.y=0;
 
         rb.AddForce(direction *moveSpeed* Time.deltaTime, ForceMode.Impulse);
     }
