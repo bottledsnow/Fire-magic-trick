503802a34bcfddb487e13c6ac4b132a51df1d32b
bottledsnow <snowmelt945@gmail.com>
2023-11-17 21:27:16 +0800

    競技場自動重新刷新，單隻敵人自動刷新(各種種類)。

--- Assets/01. Scenes/Concept_laboratory/Platform/Script/BattleArea_Enemyspawn.cs
+++ Assets/01. Scenes/Concept_laboratory/Platform/Script/BattleArea_Enemyspawn.cs
@@ -1,4 +1,6 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BattleArea_Enemyspawn : MonoBehaviour
 {
@@ -10,18 +12,64 @@ public class BattleArea_Enemyspawn : MonoBehaviour
     [SerializeField] private int EnemyNumber_B;
     [SerializeField] private Transform Enemy_C;
     [SerializeField] private int EnemyNumber_C;
+    [Header("spawnPosition")]
+    [SerializeField] private Transform Center;
+    [SerializeField] private Transform[] spawnPosition;
 
+    public void SpawnEnemy()
+    {
+        for(int i = 0;i < EnemyNumber_A;i++)
+        {
+            spawnEnemyA(spawnPosition[randomSpawnPosition()]);
+        }
+        for(int i = 0;i < EnemyNumber_B;i++)
+        {
+            spawnEnemyB(spawnPosition[randomSpawnPosition()]);
+        }
+        for(int i = 0;i < EnemyNumber_C;i++)
+        {
+            spawnEnemyC(spawnPosition[randomSpawnPosition()]);
+        }
+    }
     public void spawnEnemyA(Transform position)
     {
-        Instantiate(Enemy_A, position.position, Quaternion.identity);
+        Vector3 Position = newPosition(position);
+        Instantiate(Enemy_A, Position, newRotation(Position));
     }
     public void spawnEnemyB(Transform position)
     {
-        Instantiate(Enemy_B, position.position, Quaternion.identity);
+        Vector3 Position = newPosition(position);
+        Instantiate(Enemy_B, Position, newRotation(Position));
     }
     public void spawnEnemyC(Transform position)
     {
-        Instantiate(Enemy_C, position.position, Quaternion.identity);
+        Vector3 Position = newPosition(position);
+        Instantiate(Enemy_C, Position, newRotation(Position));
     }
+    private int randomSpawnPosition()
+    {
+        int random = UnityEngine.Random.Range(0, spawnPosition.Length);
+        return random;
+    }
+    private float RandomPosition()
+    {
+          float random = UnityEngine.Random.Range(-2f, 2f);
+        return random;
+    }
+    private Vector3 newPosition(Transform position)
+    {
+        float x = position.position.x + RandomPosition();
+        float y = position.position.y;
+        float z = position.position.z + RandomPosition();
+        Vector3 newPosition = new Vector3(x, y, z);
 
+        return newPosition;
+    }
+    private Quaternion newRotation(Vector3 newPosition)
+    {
+        Vector3 Direction = (Center.position - newPosition).normalized;
+        quaternion rotation = quaternion.LookRotation(Direction, Vector3.up);
+
+        return rotation;
+    }
 }
--- Assets/01. Scenes/Concept_laboratory/Platform/Script/EnterArea.cs
+++ Assets/01. Scenes/Concept_laboratory/Platform/Script/EnterArea.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EnterArea : MonoBehaviour
 {
     [SerializeField] private Transform TargetPositoin;
-
+        
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
