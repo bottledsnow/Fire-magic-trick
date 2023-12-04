0543d3ef678ac03269f5cd0467b280ca099619ed
Anping6630 <anping6630@gmail.com>
2023-12-05 03:46:47 +0800

    三連腳斬測試原型

---
+++ Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/EnemyA/LegSlash/LegSlash_Right.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class LegSlash_Right : Action
+{
+    [Header("SharedVariable")]
+    [SerializeField] private SharedTransform behaviorObject;
+    [SerializeField] private SharedGameObject targetObject;
+
+    [Header("AttackObject")]
+    [SerializeField] private GameObject legSlashObject;
+
+    [Header("Movement")]
+    [SerializeField] private float forwardForce;
+	[SerializeField] private float maxRotateAngle = 30;
+
+    private Transform legSlashPoint;
+    private Rigidbody rb;
+
+    public override void OnStart()
+    {
+        rb = GetComponent<Rigidbody>();
+        legSlashPoint = behaviorObject.Value.Find("LegSlashPoint");
+
+        InstantiateAttackObject();
+		Rotation();
+        Movement();
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+        return TaskStatus.Success;
+    }
+
+    private void InstantiateAttackObject()
+    {
+        GameObject slashCollider = Object.Instantiate(legSlashObject, legSlashPoint.position, legSlashPoint.rotation);
+        slashCollider.transform.parent = transform;
+    }
+
+    private void Movement()
+    {
+        rb.velocity = Vector3.zero;
+        rb.AddForce(transform.forward * forwardForce, ForceMode.Impulse);
+    }
+
+    private void Rotation()
+    {
+        // 获取敌人到玩家的方向
+        Vector3 directionToPlayer = targetObject.Value.transform.position - transform.position;
+        directionToPlayer.y = 0;
+        // 使用Quaternion.LookRotation直接设置敌人的朝向
+        Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer, Vector3.up);
+
+        // 限制转动角度不超过15度
+        float angleDifference = Quaternion.Angle(transform.rotation, targetRotation);
+        if (angleDifference <= maxRotateAngle)
+        {
+            // 如果当前角度差在限定范围内，直接设置朝向
+            transform.rotation = targetRotation;
+        }
+        else
+        {
+            // 如果超过限定范围，根据最大角度进行插值
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, maxRotateAngle / angleDifference);
+        }
+    }
+}
