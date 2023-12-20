80a4ea8641b600212c8580cf426a8bbb025f8b7c
李庠霖 <snowmelt945@gmail.com>
2023-12-20 12:26:47 +0800

    製作到一半。

--- Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/General/Movement/MoveToTarget.cs
+++ Assets/03. Character/Enemy/Script/01.BehaviorDesigner/02.CustomNode/Action/General/Movement/MoveToTarget.cs
@@ -46,6 +46,7 @@ public class MoveToTarget : Action
       Vector3 direction = (targetObject.Value.transform.position - transform.position).normalized;
       direction.y = 0;
       rb.AddForce(direction * moveSpeed * Time.deltaTime);
+      Debug.Log(rb.velocity.magnitude);
    }
    
    private void SpeedLimit()
