9149a406942efb1cdb952c111e17816239f34c5a
李庠霖 <snowmelt945@gmail.com>
2023-12-22 05:04:08 +0800

    Menu 系統正常運作

--- Assets/03. Character/Player/Script/GameMannager/Menu/MenuSystem.cs
+++ Assets/03. Character/Player/Script/GameMannager/Menu/MenuSystem.cs
@@ -1,9 +1,15 @@
+using StarterAssets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuSystem : MonoBehaviour
 {
     [Header("UI")]
     [SerializeField] private GameObject MenuUI;
+    [SerializeField] private GameObject Script;
+    [SerializeField] private GameObject UI;
+    private ThirdPersonController thirdPersonController;
+    private EnergySystem energySystem;
     private GameObject MainCamera;
     private GameManager gameManager;
     private GameObject Player;
@@ -12,16 +18,26 @@ public class MenuSystem : MonoBehaviour
     {
         gameManager = GameManager.singleton;
         Player = GameManager.singleton.Player.gameObject;
+        thirdPersonController = GameManager.singleton.Player.GetComponent<ThirdPersonController>();
+        energySystem = GameManager.singleton.Player.GetComponent<EnergySystem>();
         MainCamera = Camera.main.gameObject;
+
         Initialization();
     }
     #region For test
+    public void Test_SetMenuInterface(bool active)
+    {
+        SetMenuInterface(active);
+    }
     #endregion
     private void Initialization()
     {
         NullFatherObj(gameManager.gameObject);
         NullFatherObj(MainCamera.gameObject);
         NullFatherObj(MenuUI);
+
+        SetPlayMode(false);
+        SetMenuInterface(true);
     }
     private void NullFatherObj(GameObject obj)
     {
@@ -31,11 +47,33 @@ public class MenuSystem : MonoBehaviour
     #endregion
     private void SetPlayMode(bool active)
     {
-        Player.SetActive(active);
-
+        thirdPersonController.enabled = active;
+        energySystem.enabled = active;
+        Script.SetActive(active);
+        UI.SetActive(active);
     }
     private void SetMenuInterface(bool active)
     {
         MenuUI.gameObject.SetActive(active);
     }
+    #region MenuButton
+    public void StartGame()
+    {
+        SetMenuInterface(false);
+        SetPlayMode(true);
+    }
+    public void GameSetting()
+    {
+
+    }
+    public void Credit()
+    {
+
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+    #endregion
 }
