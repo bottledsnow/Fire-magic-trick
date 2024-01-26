9df06ce9918b150ab8c0d25da8cdbbe97e382dab
bottledsnow <snowmelt945@gmail.com>
2024-01-26 19:07:13 +0800

    Back有牌。

--- Assets/03. Character/Player/Script/GamePlayIncrease/NGP/NGP_Dash.cs
+++ Assets/03. Character/Player/Script/GamePlayIncrease/NGP/NGP_Dash.cs
@@ -51,6 +51,7 @@ public class NGP_Dash : NGP_Basic_Dash
         coolingTimer = dashCooling;
         speed = forwardDashSpeed;
         dashDistance = forwardDashDistance;
+
     }
 
     protected override void Update()
