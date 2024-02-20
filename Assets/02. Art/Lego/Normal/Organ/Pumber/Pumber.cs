c92bd2a164974e3565fba979635b8f72da198e53
bottledsnow <snowmelt945@gmail.com>
2024-02-21 00:20:19 +0800

    Pumber 4種高度變化、彈力液壓機、死亡液壓機。

---
+++ Assets/02. Art/Lego/Normal/Organ/Pumber/Pumber.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Pumber : MonoBehaviour
+{
+    //Script
+    private Animator animator;
+
+    //
+    [SerializeField] private GameObject Trigger;
+    [Header("Death")]
+    public bool isDeathPumber;
+    [Header("Bounce")]
+    public bool isBounce;
+    public float SuperPumberForce;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        Trigger.SetActive(false);
+    }
+    public void SetAnimatorState(int level)
+    {
+        animator.SetInteger("State", level);
+    }
+    public async void SuperPumber()
+    {
+        Trigger.SetActive(true);
+        await Task.Delay(250);
+        Trigger.SetActive(false);
+    }
+    public void SuperPumberReady()
+    {
+        animator.Play("Pumber Ready Super");
+    }
+    public void SuperBounce()
+    {
+        if(isBounce)
+        {
+            animator.Play("Pumber bounce");
+        }
+    }
+}
---
+++ Assets/02. Art/Lego/Normal/Organ/Pumber/TriggerArea_SuperPumber.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class TriggerArea_SuperPumber : MonoBehaviour
+{
+    //Script
+    private Pumber pumber;
+    private DeathSystem deathSystem;
+    private ImpactReceiver impactReceiver;
+    //Varable
+    private bool isDeathPumber;
+    private float SuperPumberForce;
+
+
+
+    private void Awake()
+    {
+        pumber = GetComponentInParent<Pumber>();
+        isDeathPumber = pumber.isDeathPumber;
+        SuperPumberForce = pumber.SuperPumberForce;
+    }
+    private void Start()
+    {
+        deathSystem = GameManager.singleton.UISystem.GetComponent<DeathSystem>();
+        impactReceiver = GameManager.singleton.Player.GetComponent<ImpactReceiver>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            if(isDeathPumber)
+            {
+                deathSystem.EnterDeathImage();
+            
+            }
+
+            Vector3 direction = this.transform.up;
+            Vector3 force = direction * SuperPumberForce;
+            impactReceiver.AddImpact(force);
+        }
+    }
+}
