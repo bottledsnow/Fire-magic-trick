db1192f0e9d4bed418597c340fbd36288d31a76f
Anping6630 <anping6630@gmail.com>
2024-02-21 16:48:35 +0800

    敵人視野系統新增眼高計算

--- Assets/03. Character/Enemy/Script/06.Aggro/Editor/FieldOfViewEditor.cs
+++ Assets/03. Character/Enemy/Script/06.Aggro/Editor/FieldOfViewEditor.cs
@@ -7,18 +7,19 @@ public class FieldOfViewEditor : Editor
     private void OnSceneGUI()
     {
         EnemyAggroSystem fov = (EnemyAggroSystem)target;
+        Vector3 viewPosition = new Vector3(fov.transform.position.x , fov.transform.position.y + fov.viewHeight , fov.transform.position.z);
 
         if (fov.showFovEditor)
         {
             Handles.color = Color.white;
-            Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.fovRadius);
+            Handles.DrawWireArc(viewPosition, Vector3.up, Vector3.forward, 360, fov.fovRadius);
 
             Vector3 viewAngle01 = DirectionFromAngle(fov.transform.eulerAngles.y, -fov.fovAngle / 2);
             Vector3 viewAngle02 = DirectionFromAngle(fov.transform.eulerAngles.y, fov.fovAngle / 2);
 
             Handles.color = Color.yellow;
-            Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle01 * fov.fovRadius);
-            Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngle02 * fov.fovRadius);
+            Handles.DrawLine(viewPosition, viewPosition + viewAngle01 * fov.fovRadius);
+            Handles.DrawLine(viewPosition, viewPosition + viewAngle02 * fov.fovRadius);
         }
     }
 
