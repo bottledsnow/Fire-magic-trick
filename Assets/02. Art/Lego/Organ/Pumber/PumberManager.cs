9ff9bc362ca249e19483fa444b508dce18fef55c
bottledsnow <snowmelt945@gmail.com>
2024-03-02 23:40:48 +0800

    修正教學變成牆壁

--- Assets/02. Art/Lego/Organ/Pumber/PumberManager.cs
+++ Assets/02. Art/Lego/Organ/Pumber/PumberManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
-
+[System.Serializable]
+public struct BoolParir
+{
+    public bool bool1;
+    public bool bool2;
+}
 public class PumberManager : MonoBehaviour
 {
     public Pumbers[] pumbers;
-
+    public BoolParir boolPair1;
+    public BoolParir boolPair2;
     public void CloseAllPumbers()
     {
         for(int i=0;i<pumbers.Length;i++)
